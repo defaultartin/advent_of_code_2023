24bee6e82ba1a166a14f31e1c2eed1f8ef274dfe
Artin <artinkordzangeneh@gmail.com>
2025-03-16 22:39:23 +0330

    Day 05 (Rust) -- Part 1

--- O5-rust/src/main.rs
+++ O5-rust/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let input = std::fs::read_to_string("test_first.txt").unwrap();
+    let input = std::fs::read_to_string("input.txt").unwrap();
     let mut content = input
         .split('\n')
         .filter(|s| !s.is_empty())
@@ -11,11 +11,34 @@ fn main() {
         .collect::<Vec<_>>()[1]
         .split(' ')
         .filter(|r| !r.is_empty())
-        .map(|x| x.parse::<u32>().unwrap())
+        .map(|x| x.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
     content.remove(0);
 
-    println!("{:?}", seeds);
+    let mut locations = Vec::new();
 
-    println!("{:?}", content);
+    for seed in seeds {
+        let mut to_num: i64 = seed;
+        let mut is_in_map: bool = false;
+        for line in &content {
+            if line.contains("map:") {
+                is_in_map = true;
+                continue;
+            } else {
+                if is_in_map {
+                    let map_nums = line.split(' ').map(|i| i.parse::<i64>().unwrap()).collect::<Vec<_>>();
+                    if to_num >= map_nums[1] && to_num < map_nums[1]+map_nums[2] {
+                        to_num += map_nums[0] - map_nums[1];
+                        is_in_map = false;
+                    }
+                } else {
+                    continue;
+                }
+            }
+        }
+        locations.push(to_num);
+    }
+    let min = locations.iter().min().unwrap();
+
+    println!("Part 1: {min}");
 }
